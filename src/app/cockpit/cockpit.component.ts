6680f5b16bfd9e92fbebc73571905fc1f28cff6a
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-10-11 22:59:16 +0300

    Added ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,20 +7,21 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent {
   // newServerName:string='';
-  newServerContent:string='';
+  // newServerContent:string='';
   @Output() serverCreated=new EventEmitter<{serverName:string,serverContent:string}>();
   @Output() blueprintCreated=new EventEmitter<{serverName:string,serverContent:string}>();
+  @ViewChild('serverContentInput',{static:true}) serverContentInput:ElementRef;
 
 
 
   onAddServer(nameInput:HTMLInputElement){
     this.serverCreated.emit(
-      {serverName:nameInput.value,serverContent:this.newServerContent}
+      {serverName:nameInput.value,serverContent:this.serverContentInput.nativeElement.value}
     );
   }
   onAddBluePrint(nameInput:HTMLInputElement){
     this.blueprintCreated.emit(
-      {serverName:nameInput.value,serverContent:this.newServerContent}
+      {serverName:nameInput.value,serverContent:this.serverContentInput.nativeElement.value}
     );
   }
 }
